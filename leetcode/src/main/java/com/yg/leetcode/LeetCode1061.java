1d17431dfc26ef20a903e37efc9ce24b34c96f23
liyang10 <liyang10@cmss.chinamobile.com>
2025-06-05 10:06:13 +0800

    init commit

---
+++ leetcode/src/main/java/com/yg/leetcode/LeetCode1061.java
@@ -0,0 +1,17 @@
+package com.yg.leetcode;
+
+class Solution {
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        // todo
+        return null;
+    }
+}
+
+public class LeetCode1061 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.smallestEquivalentString("aabbcc", "aabbcc", "aabbcc"));
+    }
+
+}
