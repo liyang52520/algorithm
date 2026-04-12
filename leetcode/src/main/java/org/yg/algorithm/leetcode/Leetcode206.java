6d7c8548913c34bd68f4fba6c34d5af8a7e71f06
liyang <378776683@qq.com>
2026-04-13 00:46:51 +0800

    leet code 24

---
+++ leetcode/src/main/java/org/yg/algorithm/leetcode/Leetcode206.java
@@ -0,0 +1,44 @@
+package org.yg.algorithm.leetcode;
+
+import com.sun.tools.javac.util.List;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Leetcode206 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ leetcode/src/main/java/org/yg/algorithm/leetcode/Leetcode24.java
@@ -0,0 +1,55 @@
+package org.yg.algorithm.leetcode;
+
+public class Leetcode24 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static class Solution {
+        public ListNode swapPairs(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            ListNode fakeHead = new ListNode();
+            fakeHead.next = head;
+
+            ListNode pre = fakeHead, pointer1 = head, pointer2 = head.next;
+            while (pointer1 != null && pointer2 != null) {
+                // swap
+                ListNode next = pointer2.next;
+                pre.next = pointer2;
+                pointer2.next = pointer1;
+                pointer1.next = next;
+
+                // next
+                pre = pointer1;
+                pointer1 = pre.next;
+                if (pointer1 != null){
+                    pointer2 = pointer2.next;
+                } else {
+                    pointer2 = null;
+                }
+            }
+            return fakeHead.next;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
