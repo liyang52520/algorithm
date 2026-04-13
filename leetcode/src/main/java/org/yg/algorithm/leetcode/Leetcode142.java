397f0634434c43bba14cf8008d355ee640c10a34
liyang <378776683@qq.com>
2026-04-14 01:07:33 +0800

    leet code 142, mark

---
+++ leetcode/src/main/java/org/yg/algorithm/leetcode/Leetcode142.java
@@ -0,0 +1,57 @@
+package org.yg.algorithm.leetcode;
+
+public class Leetcode142 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("val: %d, next: %s", val, next);
+        }
+    }
+
+    public static class Solution {
+        /**
+         * 没做出来
+         * @param head
+         * @return
+         */
+        public ListNode detectCycle(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {// 有环
+                    ListNode index1 = fast;
+                    ListNode index2 = head;
+                    // 两个指针，从头结点和相遇结点，各走一步，直到相遇，相遇点即为环入口
+                    while (index1 != index2) {
+                        index1 = index1.next;
+                        index2 = index2.next;
+                    }
+                    return index1;
+                }
+            }
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
